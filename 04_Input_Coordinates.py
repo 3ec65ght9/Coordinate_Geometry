e4f37340d11cad7cfdd0d09345eafa18e7b176ff
3ec65ght9 <govendera4795@masseyhigh.school.nz>
2025-07-16 14:16:18 +1200

    Add files via upload

---
+++ 04_Input_Coordinates.py
@@ -0,0 +1,18 @@
+def input_coordinates():
+    """Coordinates given by user should not be less
+     than four points"""
+
+while True:
+    print()
+
+    # ask user for first point
+    x_1 = input("x1: ")
+
+    # Ask user for second point
+    y_1 = input("y1: ")
+
+    # ask user for third point
+    x_2 = input("x2: ")
+
+    # Ask user for fourth point
+    y_2 = input("y2: ")
