17c661f77cd865bceadb7674fcbd89c1a3428e53
3ec65ght9 <govendera4795@masseyhigh.school.nz>
2025-07-30 12:24:43 +1200

    Add files via upload

---
+++ 05_Midpoint.py
@@ -0,0 +1,8 @@
+def midpoint(x1, y1, x2, y2):
+
+    mid_x = (x1 + x2)/2
+    mid_y = (y1 + y2)/2
+    return mid_x, mid_y
+
+midpoint_x_string = f"calculate mid_x: {midpoint}"
+midpoint_y_string = f"calculate mid_y: {midpoint}"
